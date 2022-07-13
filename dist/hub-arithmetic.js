4d3d7f2170e65d1f2367b0ec511f79121e154366
Mohamed <git.mhd@gmail.com>
2022-07-13 11:16:16 +0100

    Created 'improperToMixed' method.

--- dist/hub-arithmetic.js
+++ dist/hub-arithmetic.js
@@ -30,4 +30,21 @@ class HubArithmetic {
             return "N/A";
       }
    }
+
+   improperToMixed(improper) {
+      improper = improper.split("/");
+      let dividend = +improper[0],
+         divisor = +improper[1],
+         reminder = 0;
+      for (let i = 1; +improper[0] >= i; i++) {
+         if (dividend % divisor === 0) {
+            break;
+         }
+         reminder += 1;
+         dividend -= 1;
+      }
+      return `${dividend / divisor ? `${dividend / divisor} ` : ""}${
+         reminder ? `${reminder}/${divisor}` : ""
+      }`;
+   }
 }
